0526c2b25df67cf3fb7734cda8ff7d343c4a007c
Dublin Anondson <stratophone2112@gmail.com>
2019-03-10 10:21:49 -0500

    feat: Created a signup endpoint for registering new users.

--- server/api/services/login.db.service.js
+++ server/api/services/login.db.service.js
@@ -1,23 +1,41 @@
-import MongoClient from 'mongodb';
+import mongoose from 'mongoose';
 ('use strict');
-var mongodb;
+
+var UserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  handle: String,
+  email: String,
+  password: String,
+});
+
+var UserModel;
 export class LoginDatabase {
   constructor() {
-    let url = 'mongodb://'; //TODO move this to the config file once the setup is confirmed.
-    mongodb = MongoClient.connect(url, (err, db) => {
-      if (err) throw err;
-      console.log('Database created!');
-      db.close();
+    mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
     });
+    mongoose.Promise = global.Promise; //Why set this up?
+    UserModel = mongoose.model('User', UserSchema);
   }
 
   getByEmailAndPassword() {
     return Promise.resolve('Made it to the database');
   }
 
-  signupUser() {
-    Promise.resolve(mongodb.users.insert());
+  saveUser(signUpUser) {
+    let user = new UserModel();
+    user.firstName = signUpUser.firstName;
+    user.lastName = signUpUser.lastName;
+    user.handle = signUpUser.handle;
+    user.email = signUpUser.email;
+    user.password = signUpUser.password;
+
+    return user.save(function(err) {
+      if (err) return 'Something whoopsied while saving kiddo';
+    });
   }
 }
-
+//TODO write some tests for this saving business
+//TODO better handle saving to the database (ie get your promises in order and shit)
 export default new LoginDatabase();
--- server/api/services/login.service.js
+++ server/api/services/login.service.js
@@ -1,4 +1,5 @@
 import LoginDatabase from './login.db.service';
+import bcrypt from 'bcryptjs';
 ('use strict');
 
 class LoginService {
@@ -8,20 +9,37 @@ class LoginService {
 
   signup(signup) {
     //TODO setup some sanitation here.
-
-    let user = {
-      firstName: signup.firstName,
-      lastName: signup.lastName,
-      handle: signup.handle,
-      email: signup.email,
-      password: signup.password, //TODO need to salt/hash the thing
-    };
-    LoginDatabase.signupUser(user);
+    if (this.validateSignUp(signup)) {
+      let user = {
+        firstName: signup.firstName,
+        lastName: signup.lastName,
+        handle: signup.handle,
+        email: signup.email,
+        password: this.hashPassword(signup.password),
+      };
+      return Promise.resolve(LoginDatabase.saveUser(user));
+    }
+    return Promise.resolve('Your payload was empty dummy');
   }
 
   validateEmail(email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+  }
+
+  validateSignUp(signupUser) {
+    if (signupUser.firstName === '') return false;
+    else if (signupUser.lastName === '') return false;
+    else if (signupUser.handle === '') return false;
+    else if (signupUser.email === '') return false;
+    else if (signupUser.password === '') return false;
+    else return true;
+  }
+
+  hashPassword(password) {
+    let salt = bcrypt.genSaltSync(12);
+    let hash = bcrypt.hashSync(password, salt);
+    return hash;
   }
 }
 
