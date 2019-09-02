b2b4f717a15d3e4eedc6775c751dff76b1ee75f7
Danondso <Stratophone2112@gmail.com>
2019-09-02 15:09:11 -0500

    feat: added basic auth to login.

--- server/api/login/login.controller.js
+++ server/api/login/login.controller.js
@@ -2,7 +2,14 @@ import LoginService from './login.service';
 
 export class LoginController {
   login(req, res) {
-    LoginService.login(req.header('userId'), req.header('password'))
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+      return res.status(401).json({ message: 'Missing Authorization Header' });
+    }
+
+    const base64Credentials = req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+    LoginService.login(username, password)
       .then(r => {
         res
           .status(200)
