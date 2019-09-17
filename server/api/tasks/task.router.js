d1e8c381b5054999a1ec4b8693ff302848190ab9
Dublin Anondson <stratophone2112@gmail.com>
2019-09-16 19:10:22 -0500

    refactor: removed cors options from the task router and moved them to the server file.

--- server/api/tasks/task.router.js
+++ server/api/tasks/task.router.js
@@ -1,18 +1,10 @@
 import * as express from 'express';
 import taskController from './tasks.controller';
 import { authenticationRequired } from '../authentication/authentication.service';
-import cors from 'cors';
-
-var corsOptions = {
-  origin: 'localhost',
-  methods: 'POST. GET, PUT, DELETE',
-  allowedHeaders: 'authorization',
-  credentials: true,
-};
 
 export default express
   .Router()
-  .post('/task', cors(), authenticationRequired, taskController.saveTask)
+  .post('/task', authenticationRequired, taskController.saveTask)
   .put('/task/:id', authenticationRequired, taskController.updateTask)
   .delete('/task/:id', authenticationRequired, taskController.deleteTask)
   .get('/:email/tasks', authenticationRequired, taskController.getTasks);
--- server/common/server.js
+++ server/common/server.js
@@ -14,6 +14,13 @@ import cors from 'cors';
 const swaggerDocument = YAML.load('server/common/swagger/Api.yaml');
 const app = new Express();
 
+var corsOptions = {
+  origin: '*',
+  methods: 'POST, GET, PUT, DELETE',
+  allowedHeaders: 'Authorization, Content-Type',
+  credentials: true,
+};
+
 export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`);
@@ -27,7 +34,7 @@ export default class ExpressServer {
     );
     app.use(cookieParser(process.env.SESSION_SECRET));
     app.use(Express.static(`${root}/public`));
-    app.use(cors()); //TODO set cors options here
+    app.use(cors(corsOptions));
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     app.use('/api/v1/', loginRouter);
     app.use('/api/v1/', taskRouter);
